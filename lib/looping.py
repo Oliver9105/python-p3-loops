81bc460ae62ded55a46f825bf804badf661627c4
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-27 23:56:49 +0300

    Implement functions for control flow and fix errors in tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x**2 for x in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+def print_numbers_with_for():
+    for i in range(1, 6):
+        print(i)
+
+
+def print_numbers_with_while():
+    i = 1
+    while i <= 5:
+        print(i)
+        i += 1
+
+
+def square_numbers_with_comprehension():
+    return [x**2 for x in range(1, 6)]
+
+
+def square_numbers_with_generator():
+    return (x**2 for x in range(1, 6))
+
